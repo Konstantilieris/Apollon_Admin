164caf320cda429dbb860c43a054b39ac0a403e7
Lomachenko <aggeloskonstantilieris@gmail.com>
2024-02-07 17:49:55 +0200

    fixed some errors 2

--- app/(dashboard)/createbooking/[id]/page.tsx
+++ app/(dashboard)/createbooking/[id]/page.tsx
@@ -1,6 +1,6 @@
 import { getBookingById } from "@/lib/actions/booking.action";
 import { formatDate } from "@/lib/utils";
-import React from "react";
+import React, { Suspense } from "react";
 import {
   Card,
   CardContent,
@@ -12,47 +12,49 @@ import {
 
 import { getAllRooms } from "@/lib/actions/room.action";
 import EditbookingChange from "@/components/booking/EditBookingChange";
-const Page = async ({ params }: any) => {
+const page = async ({ params }: any) => {
   const booking = JSON.parse(await getBookingById(params.id));
   const rooms = JSON.parse(await getAllRooms());
   return (
-    <section className="text-dark100_light900 flex flex-col gap-4">
-      <h1 className="font-noto_sans text-[40px] font-extrabold">
-        ΚΑΛΩΣΗΡΘΑΤΕ ΣΤΗΝ ΕΠΕΞΕΡΓΑΣΙΑ ΚΡΑΤΗΣΗΣ
-      </h1>
-      <Card className="background-light800_dark400 ">
-        <CardHeader>
-          <CardTitle className="font-noto_sans font-extrabold">
-            {" "}
-            Πραγματοποιείται αλλαγή στην κράτηση με αριθμό : &nbsp; {"  "}
-            {booking._id} &nbsp;{" "}
-          </CardTitle>
-          <CardDescription className="font-noto_sans text-lg font-bold">
-            {" "}
-            Πελάτης : &nbsp;
-            {booking.clientId.lastName} &nbsp; {booking.clientId.firstName} .
-          </CardDescription>
-        </CardHeader>
-        <CardContent className="flex flex-col text-lg">
-          <span>Δωμάτιο {booking.roomId.name} </span>
-          <span>
-            Hμερομηνία και ώρα άφιξης :{" "}
-            {formatDate(new Date(booking.fromDate), "el")} στις{" "}
-            {booking.timeArrival}{" "}
-          </span>{" "}
-          <span>
-            Hμερομηνία και ώρα αναχώρησης :{" "}
-            {formatDate(new Date(booking.toDate), "el")} στις{" "}
-            {booking.timeDeparture}
-          </span>
-        </CardContent>
-        <CardFooter className="text-lg font-extrabold">
-          Προχωρήστε παρακάτω για τις εξής ενέργειες {"->"}
-        </CardFooter>
-      </Card>
-      <EditbookingChange booking={booking} rooms={rooms} />
-    </section>
+    <Suspense>
+      <section className="text-dark100_light900 flex flex-col gap-4">
+        <h1 className="font-noto_sans text-[40px] font-extrabold">
+          ΚΑΛΩΣΗΡΘΑΤΕ ΣΤΗΝ ΕΠΕΞΕΡΓΑΣΙΑ ΚΡΑΤΗΣΗΣ
+        </h1>
+        <Card className="background-light800_dark400 ">
+          <CardHeader>
+            <CardTitle className="font-noto_sans font-extrabold">
+              {" "}
+              Πραγματοποιείται αλλαγή στην κράτηση με αριθμό : &nbsp; {"  "}
+              {booking._id} &nbsp;{" "}
+            </CardTitle>
+            <CardDescription className="font-noto_sans text-lg font-bold">
+              {" "}
+              Πελάτης : &nbsp;
+              {booking.clientId.lastName} &nbsp; {booking.clientId.firstName} .
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="flex flex-col text-lg">
+            <span>Δωμάτιο {booking.roomId.name} </span>
+            <span>
+              Hμερομηνία και ώρα άφιξης :{" "}
+              {formatDate(new Date(booking.fromDate), "el")} στις{" "}
+              {booking.timeArrival}{" "}
+            </span>{" "}
+            <span>
+              Hμερομηνία και ώρα αναχώρησης :{" "}
+              {formatDate(new Date(booking.toDate), "el")} στις{" "}
+              {booking.timeDeparture}
+            </span>
+          </CardContent>
+          <CardFooter className="text-lg font-extrabold">
+            Προχωρήστε παρακάτω για τις εξής ενέργειες {"->"}
+          </CardFooter>
+        </Card>
+        <EditbookingChange booking={booking} rooms={rooms} />
+      </section>
+    </Suspense>
   );
 };
 
-export default Page;
+export default page;
--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -3,9 +3,9 @@ import React from "react";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <main className="flex relative">
-      <Sidebar/>
-      <section className="background-lightgrad_darkgrad min-h-screen w-full  p-6 max-md:pb-14 sm:px-14 custom-scrollbar">
+    <main className="relative flex">
+      <Sidebar />
+      <section className="background-lightgrad_darkgrad custom-scrollbar min-h-screen  w-full p-6 max-md:pb-14 sm:px-14">
         {children}
       </section>
     </main>
