68111332ffbf1f664da1fcf613e0debad295bac4
Lomachenko <aggeloskonstantilieris@gmail.com>
2024-08-22 21:22:23 +0300

    implement bug fixed

--- components/clientProfile/UncheckPayment.tsx
+++ components/clientProfile/UncheckPayment.tsx
@@ -1,10 +1,10 @@
 "use client";
 import React from "react";
-import { Button } from "../../ui/button";
+import { Button } from "../ui/button";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
 import { uncheckedPayment } from "@/lib/actions/client.action";
-import { useToast } from "../../ui/use-toast";
+import { useToast } from "../ui/use-toast";
 import { cn } from "@/lib/utils";
 
 const UncheckPayment = ({ clientId, item }: any) => {
