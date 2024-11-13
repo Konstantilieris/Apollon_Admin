0c15756bd8fc215ef63c2d84f25b88b3f8e3e6d5
ANGELOS KONSTANTILIERIS <aggeloskonstantilieris@gmail.com>
2024-11-14 00:06:16 +0200

    fix bug

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -1,22 +1,41 @@
 import mongoose from "mongoose";
 
-let isConnected: boolean = false;
+const globalWithMongoose = global as typeof global & {
+  mongoose: {
+    conn: typeof mongoose | null;
+    promise: Promise<typeof mongoose> | null;
+  };
+};
+
+if (!globalWithMongoose.mongoose) {
+  globalWithMongoose.mongoose = { conn: null, promise: null };
+}
 
 export const connectToDatabase = async () => {
   mongoose.set("strictQuery", true);
+
   if (!process.env.DATABASE_URL) {
-    return console.log("Missing DATABASE_URL");
+    throw new Error("Missing DATABASE_URL environment variable");
   }
-  if (isConnected) {
-    return console.log("MONGODB is already connected");
+
+  if (globalWithMongoose.mongoose.conn) {
+    console.log("Using existing Mongoose connection");
+    return globalWithMongoose.mongoose.conn;
   }
-  try {
-    await mongoose.connect(process.env.DATABASE_URL, {
-      dbName: "ApolloGuard",
-    });
-    isConnected = true;
-    console.log("Mongodb is connected");
-  } catch (error) {
-    console.log("Mongodb connection failed", error);
+
+  if (!globalWithMongoose.mongoose.promise) {
+    console.log("Establishing new Mongoose connection");
+    globalWithMongoose.mongoose.promise = mongoose.connect(
+      process.env.DATABASE_URL,
+      {
+        dbName: "ApolloGuard",
+        serverSelectionTimeoutMS: 5000, // Timeout after 5 seconds
+        socketTimeoutMS: 45000,
+      }
+    );
   }
+
+  globalWithMongoose.mongoose.conn = await globalWithMongoose.mongoose.promise;
+  console.log("New Mongoose connection established");
+  return globalWithMongoose.mongoose.conn;
 };
